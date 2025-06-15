fc6943ff0d43d9a55c4f441a667434000aad41ac
Álvaro Sánchez Bravo <a.sanbravo.developer@gmail.com>
2025-06-15 18:38:38 +0200

    [US-2] feat: update dependency

--- infrastructure-rest-client/src/test/java/com/example/demo/spotify/config/RestTemplateConfigTest.java
+++ infrastructure-rest-client/src/test/java/com/example/demo/spotify/config/RestTemplateConfigTest.java
@@ -3,6 +3,7 @@ package com.example.demo.spotify.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.client.RestTemplate;
+import com.example.demo.spotify.config.TestConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
